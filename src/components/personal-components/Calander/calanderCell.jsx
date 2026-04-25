2fba1cf863a09a22871ba6db83fc2908070242bd
love-to-code108 <love.to.code108@gmail.com>
2026-04-25 16:05:57 +0530

    working on creating new events

--- src/components/personal-components/Calander/calanderCell.jsx
+++ src/components/personal-components/Calander/calanderCell.jsx
@@ -1,13 +1,44 @@
+import { Button } from "@/components/ui/button";
 import EventCell from "./EventCell";
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+
+import { useForm, Controller } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+    Field,
+    FieldDescription,
+    FieldError,
+    FieldGroup,
+    FieldLabel,
+} from "@/components/ui/field"
+
+import { Input } from "@/components/ui/input"
+
+import {
+    InputGroup,
+    InputGroupAddon,
+    InputGroupText,
+    InputGroupTextarea,
+} from "@/components/ui/input-group"
+
+const CalanderCell = ({ value }) => {
 
 
-const CalanderCell = ({value}) => {
 
-
-
-    return(
-        <div 
-        className=" flex justify-center items-center
+    return (
+        <Dialog>
+            <DialogTrigger className={"w-full flex justify-center"}>
+                <div
+                    className=" flex justify-center items-center
         
         
         w-full min-h-[180px]
@@ -16,30 +47,65 @@ const CalanderCell = ({value}) => {
         ">
 
 
-            <div className="bg-background w-full h-full
+                    <div className="bg-background w-full h-full
             
             py-2 px-4
             hover:bg-primary-foreground
             ">
 
 
-                {/* date */}
-                <div className=" w-full flex justify-end">
-                <p className={value.isCurrentMonth ? "text-foreground":"text-border"}
-                
-                >{value.day}</p>
+                        {/* date */}
+                        <div className=" w-full flex justify-end">
+                            <p className={value.isCurrentMonth ? "text-foreground" : "text-border"}
+
+                            >{value.day}</p>
+                        </div>
+
+                        <br />
+
+
+
+
+                        {/* events */}
+                        <EventCell />
+                    </div>
+
                 </div>
-
-                <br />
-
-                
+            </DialogTrigger>
 
 
-                {/* events */}
-                <EventCell/>
-            </div>
 
-        </div>
+
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Create new event</DialogTitle>
+                    <DialogDescription>
+                        Please fill the details below
+                    </DialogDescription>
+                </DialogHeader>
+
+
+                {/* create new event form */}
+
+
+
+
+
+                <DialogFooter>
+                    <DialogClose asChild>
+                        <Button variant="outline">Cancel</Button>
+                    </DialogClose>
+                    <Button type="submit">Save changes</Button>
+                </DialogFooter>
+
+            </DialogContent>
+
+
+
+
+
+
+        </Dialog>
     )
 }
 
