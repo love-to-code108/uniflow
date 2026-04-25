8fcd13d77d6213c106545520b6bf9bb0ed676e65
love-to-code108 <love.to.code108@gmail.com>
2026-04-25 19:01:29 +0530

    chad cn ui is making me go fucking insane, why are there so many errors i dont know ahhh fuck it lets keep going for this project i would like to commit to shad cn ui

--- src/components/personal-components/Calander/calanderCell.jsx
+++ src/components/personal-components/Calander/calanderCell.jsx
@@ -1,3 +1,5 @@
+"use client"
+import React from "react";
 import { Button } from "@/components/ui/button";
 import EventCell from "./EventCell";
 import {
@@ -30,7 +32,106 @@ import {
     InputGroupTextarea,
 } from "@/components/ui/input-group"
 
+import { toast } from "sonner";
+import z from "zod";
+
+
+
+import { format } from "date-fns";
+import { Calendar as CalendarIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Calendar } from "@/components/ui/calendar";
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
+import { Textarea } from "@/components/ui/textarea";
+
+
+
+
+
+
+// form schema
+const formSchema = z.object({
+    "eventName": z
+        .string(),
+
+    "eventDescription": z
+        .string(),
+
+    "eventDate": z
+        .string(),
+
+    "startTime": z
+        .string(),
+
+    "endTime": z
+        .string(),
+
+    "registrationLink": z
+        .string(),
+
+    "expectedNumberOfStudents": z
+        .string(),
+
+})
+
+
+
+
+
+
+
+
+
+
+
 const CalanderCell = ({ value }) => {
+
+    // console.log(value)
+    const preFilledDate = new Date(value.year, value.month, value.day);
+    const form = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            "eventName": "",
+            "eventDescription": "",
+            eventDate: preFilledDate,
+            "startTime": "",
+            "endTime": "",
+            "registrationLink": "",
+            "expectedNumberOfStudents": "",
+        }
+    })
+
+
+
+
+
+    const [date, setDate] = React.useState(preFilledDate)
+
+
+
+
+
+
+    // function that runs on form submit
+    const onSubmit = async (data) => {
+
+        console.log("onsubmit running")
+        console.log(data);
+    }
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -75,7 +176,7 @@ const CalanderCell = ({ value }) => {
 
 
 
-
+            {/* dialog header */}
             <DialogContent>
                 <DialogHeader>
                     <DialogTitle>Create new event</DialogTitle>
@@ -86,17 +187,264 @@ const CalanderCell = ({ value }) => {
 
 
                 {/* create new event form */}
+                <form id="createNewEventForm"
+                    onSubmit={form.handleSubmit(onSubmit)}>
+                    <FieldGroup className={"gap-3"}>
+
+
+
+                        {/* event name */}
+                        <Controller
+                            name="eventName"
+                            control={form.control}
+                            render={({ field, fieldState }) => (
+
+                                <Field data-invalid={fieldState.invalid}>
+
+
+                                    <FieldLabel htmlFor="createNewEventEventName">
+                                        Event Name
+                                    </FieldLabel>
+
+
+                                    <Input
+                                        {...field}
+                                        id="createNewEventEventName"
+                                        aria-invalid={fieldState.invalid}
+                                        placeholder="Event Name"
+                                        autoComplete="off"
+                                    />
+                                    {fieldState.invalid && (
+                                        <FieldError errors={[fieldState.error]} />
+                                    )}
+                                </Field>
+                            )}
+                        />
 
 
 
 
 
-                <DialogFooter>
-                    <DialogClose asChild>
-                        <Button variant="outline">Cancel</Button>
-                    </DialogClose>
-                    <Button type="submit">Save changes</Button>
-                </DialogFooter>
+
+                        {/* event description */}
+                        <Controller
+                            name="eventDescription"
+                            control={form.control}
+                            render={({ field, fieldState }) => (
+
+                                <Field data-invalid={fieldState.invalid}>
+
+
+                                    <FieldLabel htmlFor="eventDescription">
+                                        Event Description
+                                    </FieldLabel>
+
+
+                                    <Textarea
+                                        {...field}
+                                        id="eventDescription"
+                                        aria-invalid={fieldState.invalid}
+                                        placeholder="Event Description"
+                                        autoComplete="off"
+                                    />
+                                    {fieldState.invalid && (
+                                        <FieldError errors={[fieldState.error]} />
+                                    )}
+                                </Field>
+                            )}
+                        />
+
+
+
+
+                        {/* date */}
+                        <Controller
+                            control={form.control}
+                            name="eventDate"
+                            rules={{ required: "Date is required" }}
+                            render={({ field }) => (
+                                <Popover>
+                                    <FieldLabel htmlFor="eventDate">
+                                        Event Date
+                                    </FieldLabel>
+                                    <PopoverTrigger asChild>
+                                        <div className=" w-full flex justify-start">
+                                            <Button
+                                                variant={"outline"}
+                                                className={cn(
+                                                    " justify-start text-left font-normal",
+                                                    !field.value && "text-muted-foreground"
+                                                )}
+                                            >
+                                                <CalendarIcon className="mr-2 h-4 w-4" />
+                                                {field.value ? format(field.value, "PPP") : <span>Pick a date</span>}
+                                            </Button>
+                                        </div>
+                                    </PopoverTrigger>
+                                    <PopoverContent className="w-auto p-0" align="start">
+                                        <Calendar
+                                            id="eventDate"
+                                            mode="single"
+                                            selected={date}
+                                            onSelect={field.onChange} // Updates the form state directly
+                                            disabled={(date) =>
+                                                date < new Date() || date < new Date("1900-01-01")
+                                            }
+
+                                        />
+                                    </PopoverContent>
+                                </Popover>
+                            )}
+                        />
+
+
+
+                        {/* start time */}
+                        <Controller
+                            name="startTime"
+                            control={form.control}
+                            render={({ field, fieldState }) => (
+
+                                <Field data-invalid={fieldState.invalid}>
+
+
+                                    <FieldLabel htmlFor="startTime">
+                                        Start Time
+                                    </FieldLabel>
+
+                                    <div className=" w-full flex justify-start">
+                                        <Input
+
+                                            type="time"
+                                            id="startTime"
+                                            aria-invalid={fieldState.invalid}
+                                            step="1"
+                                            defaultValue="10:30:00"
+                                            className="
+                                    w-[90px]
+                                    appearance-none bg-background [&::-webkit-calendar-picker-indicator]:hidden [&::-webkit-calendar-picker-indicator]:appearance-none"
+                                        />
+                                    </div>
+                                    {fieldState.invalid && (
+                                        <FieldError errors={[fieldState.error]} />
+                                    )}
+                                </Field>
+                            )}
+                        />
+
+
+
+
+
+                        {/* end time */}
+                        <Controller
+                            name="endTime"
+                            control={form.control}
+                            render={({ field, fieldState }) => (
+
+                                <Field data-invalid={fieldState.invalid}>
+
+
+                                    <FieldLabel htmlFor="endTime">
+                                        End Time
+                                    </FieldLabel>
+
+                                    <div className=" w-full flex ">
+                                        <Input
+
+                                            type="time"
+                                            id="endTime"
+                                            aria-invalid={fieldState.invalid}
+                                            step="1"
+                                            defaultValue="12:30:00"
+                                            className="
+                                    w-[90px]
+                                    appearance-none bg-background [&::-webkit-calendar-picker-indicator]:hidden [&::-webkit-calendar-picker-indicator]:appearance-none"
+                                        />
+                                    </div>
+                                    {fieldState.invalid && (
+                                        <FieldError errors={[fieldState.error]} />
+                                    )}
+                                </Field>
+                            )}
+                        />
+
+
+
+
+
+
+                        {/* registration link */}
+                        <Controller
+                            name="registrationLink"
+                            control={form.control}
+                            render={({ field, fieldState }) => (
+
+                                <Field data-invalid={fieldState.invalid}>
+
+
+                                    <FieldLabel htmlFor="registrationLink">
+                                        Registration Link
+                                    </FieldLabel>
+
+
+                                    <Input
+                                        {...field}
+                                        id="registrationLink"
+                                        aria-invalid={fieldState.invalid}
+                                        autoComplete="off"
+                                        placeholder="https://api.example.com/webhook"
+                                    />
+                                    {fieldState.invalid && (
+                                        <FieldError errors={[fieldState.error]} />
+                                    )}
+                                </Field>
+                            )}
+                        />
+
+
+
+
+
+                        {/* expected number of students */}
+                        <Controller
+                            name="expectedNumberOfStudents"
+                            control={form.control}
+                            render={({ field, fieldState }) => (
+
+                                <Field data-invalid={fieldState.invalid}>
+
+
+                                    <FieldLabel htmlFor="expectedNumberOfStudents">
+                                        Expected number of students
+                                    </FieldLabel>
+
+
+                                    <Input
+                                        {...field}
+                                        id="expectedNumberOfStudents"
+                                        aria-invalid={fieldState.invalid}
+                                        autoComplete="off"
+                                    />
+                                    {fieldState.invalid && (
+                                        <FieldError errors={[fieldState.error]} />
+                                    )}
+                                </Field>
+                            )}
+                        />
+
+                    </FieldGroup>
+
+
+
+                    <Button type="submit"
+                        form="createNewEventForm">Create Event</Button>
+
+
+
+
+
+                </form>
 
             </DialogContent>
 
